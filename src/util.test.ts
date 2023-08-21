ca1f396bc22cb43bb96df9cf19288e714760971b
mizdra <pp.mizdra@gmail.com>
2023-08-22 00:48:07 +0900

    add test cases

--- src/util.test.ts
+++ src/util.test.ts
@@ -2,9 +2,19 @@ import { expectTypeOf, it } from 'vitest';
 import { type DeepOptional, type Merge } from './util.js';
 
 it('DeepOptional', () => {
-  type Input = { a: number; b: { c: number }; d: undefined };
+  type Input = {
+    a: number;
+    b: { c: number };
+    d: undefined;
+    e: { f: number }[];
+  };
   type Actual = DeepOptional<Input>;
-  type Expected = { a: number | undefined; b: { c: number | undefined } | undefined; d: undefined };
+  type Expected = {
+    a: number | undefined;
+    b: { c: number | undefined } | undefined;
+    d: undefined;
+    e: { f: number | undefined }[] | undefined;
+  };
   expectTypeOf<Actual>().toEqualTypeOf<Expected>();
 });
 
