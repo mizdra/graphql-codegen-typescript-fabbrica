f895ebb95f2795df2fd98c5a0ed603f715bba373
mizdra <pp.mizdra@gmail.com>
2024-03-11 01:13:19 +0900

    remove unused codes

--- e2e/cjs/test/util.ts
+++
@@ -1,6 +0,0 @@
-import { randomInt } from 'node:crypto';
-
-export function oneOf<const T>(items: T[]): T {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  return items[randomInt(items.length)]!;
-}
