f321c0766c03b0f9f424d2d59c6580e990e04ddf
mizdra <pp.mizdra@gmail.com>
2024-09-16 00:30:25 +0900

    create default config file for E2E tests

--- e2e/01-esm/codegen.ts
+++ e2e/01-esm/codegen.ts
@@ -1,15 +1,5 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
-
-const defaultTypeScriptPluginConfig = {
-  nonOptionalTypename: true,
-  enumsAsTypes: true,
-  avoidOptionals: true,
-  skipTypename: true,
-};
-const defaultFabbricaPluginConfig = {
-  typesFile: './types.js',
-  skipTypename: true,
-};
+import { defaultFabbricaPluginConfig, defaultTypeScriptPluginConfig } from '../util/config.cjs';
 
 const config: CodegenConfig = {
   generates: {
@@ -18,6 +8,8 @@ const config: CodegenConfig = {
       plugins: ['typescript'],
       config: {
         ...defaultTypeScriptPluginConfig,
+        nonOptionalTypename: true,
+        skipTypename: true,
         scalars: {
           CustomScalarTest_CustomScalar1: 'Date',
           CustomScalarTest_CustomScalar2: '{ field: string }',
@@ -32,6 +24,7 @@ const config: CodegenConfig = {
       plugins: ['@mizdra/graphql-codegen-typescript-fabbrica'],
       config: {
         ...defaultFabbricaPluginConfig,
+        skipTypename: true,
         namingConvention: {
           typeNames: './my-naming-fn.cjs',
         },
---
+++ e2e/util/config.cts
@@ -0,0 +1,7 @@
+export const defaultTypeScriptPluginConfig = {
+  enumsAsTypes: true, // required
+  avoidOptionals: true, // required
+};
+export const defaultFabbricaPluginConfig = {
+  typesFile: './types.js', // required
+};
